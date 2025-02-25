cba8af756efcf73550bec67acbc8f0e95a63a428
NguyenHoaiPhong <nguyenhoaiphong14723@gmail.com>
2025-02-25 15:29:42 +0700

    [update]-add filter by role in getAllUsers

--- src/database/repository/UserRepo.ts
+++ src/database/repository/UserRepo.ts
@@ -10,6 +10,7 @@ import KeystoreRepo from './KeystoreRepo';
 import prisma from '../prismaClient';
 import { User } from '@prisma/client';
 import { find } from 'lodash';
+import role from '../../helpers/role';
 
 async function exists(id: Types.ObjectId): Promise<boolean> {
   const user = await UserModel.exists({ _id: id, status: true });
@@ -260,6 +261,37 @@ async function findAll() {
   });
 }
 
+async function findByRole(roleCode: string) {
+  return await prisma.user.findMany({
+    where: {
+      roles: {
+        some: {
+          code: roleCode,
+        },
+      },
+    },
+    select: {
+      id: true,
+      email: true,
+      name: true,
+      profilePicUrl: true,
+      verified: true,
+      status: true,
+      createdAt: true,
+      updatedAt: true,
+      address: true,
+      city: true,
+      country: true,
+      dateOfBirth: true,
+      gender: true,
+      firstName: true,
+      lastName: true,
+      roles: true,
+      // password field is omitted
+    }
+  });
+}
+
 export default {
   exists,
   findPrivateProfileById,
@@ -272,4 +304,5 @@ export default {
   updateInfo,
   activeAccount,
   findAll,
+  findByRole,
 };
